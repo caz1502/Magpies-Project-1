f9944ee4e475929b52b10fb6ca6353a08238047e
Carl Santiago <4518gg@gmail.com>
2021-10-24 14:46:53 +1030

    Added more features for news section

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -85,31 +85,31 @@ var getInfo = function (){
             newsSelectedCategories = [];
             if(document.getElementById("newsGeneral").checked)
             {
-                newsSelectedCategories.push("General");
+                newsSelectedCategories.push("general");
             }
             if(document.getElementById("newsBusiness").checked)
             {
-                newsSelectedCategories.push("Business");
+                newsSelectedCategories.push("business");
             }
             if(document.getElementById("newsEntertainment").checked)
             {
-                newsSelectedCategories.push("Entertainment");
+                newsSelectedCategories.push("entertainment");
             }
             if(document.getElementById("newsHealth").checked)
             {
-                newsSelectedCategories.push("Health");
+                newsSelectedCategories.push("health");
             }
             if(document.getElementById("newsScience").checked)
             {
-                newsSelectedCategories.push("Science");
+                newsSelectedCategories.push("science");
             }
             if(document.getElementById("newsSports").checked)
             {
-                newsSelectedCategories.push("Sports");
+                newsSelectedCategories.push("sports");
             }
             if(document.getElementById("newsTechnology").checked)
             {
-                newsSelectedCategories.push("Technology");
+                newsSelectedCategories.push("technology");
             }
             if(newsSelectedCategories.length == 0)
             {
